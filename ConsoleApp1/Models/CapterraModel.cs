2abd94ab996a3275153e5446c288fd5a7fe47c01
hiteshkalra4 <34619733+hiteshkalra4@users.noreply.github.com>
2022-04-11 16:01:31 +0530

    Initial Commit

---
+++ ConsoleApp1/Models/CapterraModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    public class CapterraModel
+    {
+        public string tags { get; set; }
+        public string name { get; set; }
+        public string twitter { get; set; }
+    }
+              
+}
---
+++ ConsoleApp1/Models/SoftwareadviceModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    public class SoftwareadviceModel
+    {
+            public List<SoftwareadviceProduct> products { get; set; }
+    }
+
+    public class SoftwareadviceProduct
+    {
+        public List<string> categories { get; set; }
+        public string twitter { get; set; }
+        public string title { get; set; }
+    }
+
+    public class SoftwareadviceResponseModel
+    {
+        public string twitter { get; set; }
+        public string categories { get; set; }
+        public string name { get; set; }
+    }
+}
